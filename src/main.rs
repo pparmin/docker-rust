0595554276ddc2ab2ecb614f4f7be287fd50e3cf
Philipp Armingeon <philipp.armingeon@aiven.io>
2022-11-17 23:44:14 +0100

    solution for second stage

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     // Uncomment this block to pass the first stage!
     let args: Vec<_> = std::env::args().collect();
+
     let command = &args[3];
     let command_args = &args[4..];
+
     let output = std::process::Command::new(command)
         .args(command_args)
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::piped())
         .output()
-        .unwrap();
-    
+        .expect("process failed to start");
+
     if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout).unwrap();
-        println!("{}", std_out)
+        let std_err = std::str::from_utf8(&output.stderr).unwrap();
+        print!("{}", std_out);
+        eprint!("{}", std_err);
     } else {
         std::process::exit(1);
     }
